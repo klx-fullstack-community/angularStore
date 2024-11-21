90a59ad53dd9c9bdb2b880af111848acd0bd5300
GPASSOS <gustavo.passos@caceis.com>
2024-11-21 13:40:11 +0000

    cahnge readme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'olis-v16'`, () => {
+  it(`should have as title 'ktech'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('olis-v16');
+    expect(app.title).toEqual('ktech');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('olis-v16 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ktech app is running!');
   });
 });
